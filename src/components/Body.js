72b99fef3490883b5d3f6607d9c8212cd859f929
sunabhasinha <mailsunabha@gmail.com>
2023-03-09 02:19:49 +0530

    added shimme. Added search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,56 +1,78 @@
-import { restaurantList } from "../contants";
-import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-
-// What is state
-// what is React Hooks? - functions,
-// What is useState
+import { restaurantList } from '../contants';
+import RestaurantCard from './RestaurantCard';
+import { useEffect, useState } from 'react';
+import Shimmer from './Shimmer';
 
 function filterData(searchText, restaurants) {
-  const filterData = restaurants.filter((restaurant) =>
-    restaurant.data.name.includes(searchText)
-  );
+	const filterData = restaurants.filter((restaurant) =>
+		restaurant.data.name.includes(searchText)
+	);
 
-  return filterData;
+	return filterData;
 }
 
 const Body = () => {
-  const [restaurants, setRestaurants] = useState(restaurantList);
-  const [searchText, setSearchText] = useState("");
+	const [filteredREstaurants, setFilteredRestaurants] = useState([]);
+	const [allRestaurants, setAllRestaurants] = useState([]);
+	const [searchText, setSearchText] = useState('');
 
-  return (
-    <>
-      <div className="search-container">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Search"
-          value={searchText}
-          onChange={(e) => {
-            setSearchText(e.target.value);
-          }}
-        />
-        <button
-          className="search-btn"
-          onClick={() => {
-            //need to filter the data
-            const data = filterData(searchText, restaurants);
-            // update the state - restaurants
-            setRestaurants(data);
-          }}
-        >
-          Search
-        </button>
-      </div>
-      <div className="restaurant-list">
-        {restaurants.map((restaurant) => {
-          return (
-            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
-          );
-        })}
-      </div>
-    </>
-  );
+	/**
+	 * first component renders then useEffect is called.
+	 * empty dependency array => once after initial render
+	 * dep array [searchText] => once after initial render + everytime after render (searchText changes)
+	 */
+	useEffect(() => {
+		getRestaurantList();
+	}, []);
+
+	async function getRestaurantList() {
+		const data = await fetch(
+			'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING'
+		);
+		const json = await data.json();
+		setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+		setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+	}
+
+	return allRestaurants.length === 0 ? (
+		<Shimmer />
+	) : (
+		<>
+			<div className="search-container">
+				<input
+					type="text"
+					className="search-input"
+					placeholder="Search"
+					value={searchText}
+					onChange={(e) => {
+						setSearchText(e.target.value);
+					}}
+				/>
+				<button
+					className="search-btn"
+					onClick={() => {
+						//need to filter the data
+						const data = filterData(searchText, allRestaurants);
+						// update the state - restaurants
+						setFilteredRestaurants(data);
+					}}
+				>
+					Search
+				</button>
+			</div>
+			<div className="restaurant-list">
+				{filteredREstaurants.length ? (
+					filteredREstaurants.map((restaurant) => {
+						return (
+							<RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+						);
+					})
+				) : (
+					<h1>No Filtered Restaurants</h1>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default Body;
